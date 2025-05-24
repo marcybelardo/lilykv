61a30dd01934219b97ac24f7d15279f634c769b3
Marceline Belardo <marcelinebelardo@gmail.com>
2025-05-24 14:00:27 +0300

    de/serializing done, basically

--- src/parser.rs
+++ src/parser.rs
@@ -7,78 +7,105 @@ pub enum Resp {
     Arr(Vec<Resp>),
 }
 
+pub struct ParserCursor<'a> {
+    input: &'a [u8],
+    pos: usize,
+}
+
+impl<'a> ParserCursor<'a> {
+    pub fn new(input: &'a str) -> Self {
+        Self { input: input.as_bytes(), pos: 0 }
+    }
+
+    pub fn peek(&self) -> Option<u8> {
+        self.input.get(self.pos).copied()
+    }
+
+    pub fn step(&mut self, n: usize) {
+        self.pos += n;
+    }
+
+    pub fn next_line(&mut self) -> Option<&'a str> {
+        let slice = &self.input[self.pos..];
+        let newline_pos = slice.windows(2).position(|w| w == b"\r\n")?;
+        let line = &slice[..newline_pos];
+        self.pos += newline_pos + 2;
+        std::str::from_utf8(line).ok()
+    }
+
+    pub fn consume_bytes(&mut self, len: usize) -> Option<String> {
+        let end = self.pos + len;
+        if end + 2 > self.input.len() { return None }
+
+        let data = &self.input[self.pos..end];
+        let suffix = &self.input[end..end + 2];
+
+        if suffix != b"\r\n" { return None }
+
+        self.pos += end + 2;
+
+        std::str::from_utf8(data).ok().map(|s| s.to_owned())
+    }
+}
+
 pub struct Parser;
 
 impl Parser {
-    pub fn deserialize(data: &str) -> Option<Resp> {
-        let mut input = data;
-        Self::parse(&mut input)
+    pub fn deserialize(data: String) -> Option<Resp> {
+        let mut cursor = ParserCursor::new(&data);
+        Self::parse_from_str(&mut cursor)
     }
 
-    fn parse(input: &mut &str) -> Option<Resp> {
-        let (prefix, rest) = input.split_at(1);
-        *input = rest;
-
-        match prefix {
-            "+" => Some(Resp::SimpStr(Self::read_line(input)?)),
-            "-" => Some(Resp::Err(Self::read_line(input)?)),
-            ":" => Self::read_line(input)?.parse::<i64>().ok().map(Resp::Int),
-            "$" => {
-                let len_line = Self::read_line(input)?;
-                let len: usize = len_line.parse().ok()?;
-                let (bulk, rest) = input.split_at(len + 2);
-                let (bulk_str, term) = bulk.split_at(len);
-                if term != "\r\n" {
-                    return None;
-                }
-                *input = rest;
-                Some(Resp::BulkStr(bulk_str.to_owned()))
-            }
-            "*" => {
-                let count_line = Self::read_line(input)?;
-                let count: usize = count_line.parse().ok()?;
-                let mut elems = Vec::with_capacity(count);
-                for _ in 0..count {
-                    let elem = Self::parse(input)?;
-                    elems.push(elem);
-                }
-                Some(Resp::Arr(elems))
-            }
-            _ => None,
-        }
-    }
-
-    fn read_line(input: &mut &str) -> Option<String> {
-        if let Some(pos) = input.find("\r\n") {
-            let (line, rest) = input.split_at(pos);
-            *input = &rest[2..];
-            Some(line.to_owned())
-        } else {
-            None
-        }
-    }
-
-    pub fn serialize(data: Resp) -> Option<String> {
-        match data {
-            Resp::SimpStr(str) => Some(format!("+{str}\r\n")),
-            Resp::Err(str) => Some(format!("-{str}\r\n")),
-            Resp::Int(num) => Some(format!(":{num}\r\n")),
-            Resp::BulkStr(str) => Some(format!("${}\r\n{}\r\n", str.len(), str)),
-            Resp::Arr(elems) => {
+    pub fn serialize(resp_type: Resp) -> Option<String> {
+        match resp_type {
+            Resp::SimpStr(s) => Some(format!("+{s}\r\n")),
+            Resp::Err(s) => Some(format!("-{s}\r\n")),
+            Resp::Int(n) => Some(format!(":{n}\r\n")),
+            Resp::BulkStr(s) => Some(format!("${}\r\n{}\r\n", s.len(), s)),
+            Resp::Arr(v) => {
                 let mut out = String::new();
-                let mut count = 0;
-                for elem in elems {
-                    if let Some(serialized) = Parser::serialize(elem) {
-                        out.push_str(&format!("{}\r\n", serialized));
-                    } else {
-                        return None;
-                    }
-                    count += 1;
+                let count = v.len();
+                for elem in v {
+                    out.push_str(&Self::serialize(elem)?);
                 }
-                out.insert_str(0, &format!("*{count}\r\n"));
-
-                Some(out)
+                Some(format!("*{}\r\n{}", count, out))
             }
+        }
+    }
+
+    fn parse_from_str(cursor: &mut ParserCursor) -> Option<Resp> {
+        match cursor.peek() {
+            Some(b'+') => {
+                cursor.step(1);
+                Some(Resp::SimpStr(cursor.next_line()?.to_owned()))
+            }
+            Some(b'-') => {
+                cursor.step(1);
+                Some(Resp::Err(cursor.next_line()?.to_owned()))
+            }
+            Some(b':') => {
+                cursor.step(1);
+                cursor.next_line()?.parse::<i64>().ok().map(Resp::Int)
+            }
+            Some(b'$') => {
+                cursor.step(1);
+                let len = cursor.next_line()?.parse::<usize>().ok()?;
+                let line = cursor.consume_bytes(len)?;
+                Some(Resp::BulkStr(line))
+            }
+            Some(b'*') => {
+                cursor.step(1);
+                let elems = cursor.next_line()?.parse::<usize>().ok()?;
+                let mut resp_vec: Vec<Resp> = Vec::with_capacity(elems);
+                for _ in 0..elems {
+                    let elem = Self::parse_from_str(cursor)?;
+                    println!("Pushing {:?}", elem);
+                    resp_vec.push(elem);
+                }
+
+                Some(Resp::Arr(resp_vec))
+            }
+            _ => None
         }
     }
 }
@@ -89,50 +116,169 @@ mod tests {
 
     #[test]
     fn deserialize_simple_string() {
-        let test = String::from("+hello world\r\n");
-        let parsed = Parser::deserialize(&test);
-        assert_eq!(parsed, Some(Resp::SimpStr(String::from("hello world"))));
+        let test = String::from("+OK\r\n");
+        let expected = Some(Resp::SimpStr(String::from("OK")));
+        assert_eq!(Parser::deserialize(test), expected);
     }
 
     #[test]
     fn deserialize_error() {
-        let test = String::from("-ERR test error\r\n");
-        let parsed = Parser::deserialize(&test);
-        assert_eq!(parsed, Some(Resp::Err(String::from("ERR test error"))));
+        let test = String::from("-ERR something bad\r\n");
+        let expected = Some(Resp::Err(String::from("ERR something bad")));
+        assert_eq!(Parser::deserialize(test), expected);
     }
 
     #[test]
-    fn deserialize_int() {
-        let test = String::from(":75\r\n");
-        let parsed = Parser::deserialize(&test);
-        assert_eq!(parsed, Some(Resp::Int(75)));
+    fn deserialize_integer() {
+        let test = String::from(":42\r\n");
+        let expected = Some(Resp::Int(42));
+        assert_eq!(Parser::deserialize(test), expected);
     }
 
     #[test]
     fn deserialize_bulk_string() {
-        let test = String::from("$12\r\n1\r\nanother\r\n");
-        let parsed = Parser::deserialize(&test);
-        assert_eq!(parsed, Some(Resp::BulkStr(String::from("1\r\nanother\r\n"))));
+        let test = String::from("$6\r\nfoobar\r\n");
+        let expected = Some(Resp::BulkStr(String::from("foobar")));
+        assert_eq!(Parser::deserialize(test), expected);
     }
 
     #[test]
-    fn deserialize_array() {
-        let test = String::from("*2\r\n+hello\r\n:7\r\n");
-        let parsed = Parser::deserialize(&test);
-        assert_eq!(parsed, Some(Resp::Arr(vec![Resp::SimpStr(String::from("hello")), Resp::Int(7)])));
+    fn deserialize_array_of_simple_strings() {
+        let test = String::from("*2\r\n+foo\r\n+bar\r\n");
+        let expected = Some(Resp::Arr(vec![
+            Resp::SimpStr(String::from("foo")),
+            Resp::SimpStr(String::from("bar")),
+        ]));
+        assert_eq!(Parser::deserialize(test), expected);
+    }
+
+    #[test]
+    fn deserialize_array_of_mixed_types() {
+        let test = String::from("*3\r\n+hello\r\n:123\r\n$5\r\nworld\r\n");
+        let expected = Some(Resp::Arr(vec![
+            Resp::SimpStr(String::from("hello")),
+            Resp::Int(123),
+            Resp::BulkStr(String::from("world")),
+        ]));
+        assert_eq!(Parser::deserialize(test), expected);
+    }
+
+    #[test]
+    fn deserialize_nested_array() {
+        let test = String::from("*2\r\n*2\r\n+foo\r\n+bar\r\n:42\r\n");
+        let expected = Some(Resp::Arr(vec![
+            Resp::Arr(vec![
+                Resp::SimpStr(String::from("foo")),
+                Resp::SimpStr(String::from("bar")),
+            ]),
+            Resp::Int(42),
+        ]));
+        assert_eq!(Parser::deserialize(test), expected);
+    }
+
+    #[test]
+    fn deserialize_empty_array() {
+        let test = String::from("*0\r\n");
+        let expected = Some(Resp::Arr(vec![]));
+        assert_eq!(Parser::deserialize(test), expected);
+    }
+
+    #[test]
+    fn deserialize_empty_bulk_string() {
+        let test = String::from("$0\r\n\r\n");
+        let expected = Some(Resp::BulkStr(String::new()));
+        assert_eq!(Parser::deserialize(test), expected);
+    }
+
+    #[test]
+    fn deserialize_invalid_prefix() {
+        let test = String::from("!oops\r\n");
+        assert_eq!(Parser::deserialize(test), None);
+    }
+
+    #[test]
+    fn deserialize_invalid_integer() {
+        let test = String::from(":notanint\r\n");
+        assert_eq!(Parser::deserialize(test), None);
+    }
+
+    #[test]
+    fn deserialize_truncated_bulk_string() {
+        let test = String::from("$4\r\nfoo\r\n"); // Length is 4 but only 3 bytes in "foo"
+        assert_eq!(Parser::deserialize(test), None);
     }
 
     #[test]
     fn serialize_simple_string() {
-        let test = Resp::SimpStr(String::from("hello!"));
-        let parsed = Parser::serialize(test);
-        assert_eq!(parsed, Some(String::from("+hello!\r\n")));
+        let data = Resp::SimpStr(String::from("OK"));
+        let test = Parser::serialize(data);
+        assert_eq!(test, Some(String::from("+OK\r\n")));
     }
 
     #[test]
     fn serialize_error() {
-        let test = Resp::Err(String::from("Bad error"));
-        let parsed = Parser::serialize(test);
-        assert_eq!(parsed, Some(String::from("-Bad error\r\n")));
+        let data = Resp::Err(String::from("ERR unknown command"));
+        let test = Parser::serialize(data);
+        assert_eq!(test, Some(String::from("-ERR unknown command\r\n")));
+    }
+
+    #[test]
+    fn serialize_integer() {
+        let data = Resp::Int(1000);
+        let test = Parser::serialize(data);
+        assert_eq!(test, Some(String::from(":1000\r\n")));
+    }
+
+    #[test]
+    fn serialize_bulk_string() {
+        let data = Resp::BulkStr(String::from("foobar"));
+        let test = Parser::serialize(data);
+        assert_eq!(test, Some(String::from("$6\r\nfoobar\r\n")));
+    }
+
+    #[test]
+    fn serialize_empty_bulk_string() {
+        let data = Resp::BulkStr(String::from(""));
+        let test = Parser::serialize(data);
+        assert_eq!(test, Some(String::from("$0\r\n\r\n")));
+    }
+
+    #[test]
+    fn serialize_array_of_integers() {
+        let data = Resp::Arr(vec![
+            Resp::Int(1),
+            Resp::Int(2),
+            Resp::Int(3),
+        ]);
+        let test = Parser::serialize(data);
+        assert_eq!(test, Some(String::from("*3\r\n:1\r\n:2\r\n:3\r\n")));
+    }
+
+    #[test]
+    fn serialize_array_of_mixed_types() {
+        let data = Resp::Arr(vec![
+            Resp::SimpStr(String::from("hello")),
+            Resp::Int(42),
+            Resp::BulkStr(String::from("foo")),
+        ]);
+        let test = Parser::serialize(data);
+        assert_eq!(test, Some(String::from("*3\r\n+hello\r\n:42\r\n$3\r\nfoo\r\n")));
+    }
+
+    #[test]
+    fn serialize_nested_array() {
+        let data = Resp::Arr(vec![
+            Resp::Int(1),
+            Resp::Arr(vec![
+                Resp::BulkStr(String::from("foo")),
+                Resp::BulkStr(String::from("bar")),
+            ]),
+            Resp::Int(2),
+        ]);
+        let test = Parser::serialize(data);
+        assert_eq!(
+            test,
+            Some(String::from("*3\r\n:1\r\n*2\r\n$3\r\nfoo\r\n$3\r\nbar\r\n:2\r\n"))
+        );
     }
 }
