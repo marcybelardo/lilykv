bcf93a07bd77202d28fb56789cbad45d35de98d5
Marceline Belardo <marcelinebelardo@gmail.com>
2025-05-25 18:56:26 +0300

    using tokio for the tcp server

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,34 @@
-use std::io::{BufReader, prelude::*};
-use std::net::{TcpListener, TcpStream};
-mod parser;
-// use parser::Resp;
-// use parser::Parser;
+use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use std::error::Error;
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
+    loop {
+        let (mut socket, _) = listener.accept().await?;
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        handle_conn(stream);
+        tokio::spawn(async move {
+            let mut buf = [0u8; 1024];
+
+            loop {
+                let n = match socket.read(&mut buf).await {
+                    // socket closed
+                    Ok(0) => return,
+                    Ok(n) => n,
+                    Err(e) => {
+                        eprintln!("failed to read socket; err = {:?}", e);
+                        return;
+                    }
+                };
+
+                println!("{}", String::from_utf8_lossy(&buf[0..n]));
+
+                if let Err(e) = socket.write_all(&buf[0..n]).await {
+                    eprintln!("failed to write to socket; err = {:?}", e);
+                    return;
+                }
+            }
+        });
     }
-}
-
-fn handle_conn(stream: TcpStream) {
-    let mut buf_reader = BufReader::new(&stream);
-    let mut buf = String::new();
-    let bytes_read = buf_reader.read_to_string(&mut buf).unwrap();
-    assert_eq!(bytes_read, buf.len());
-
-    println!("{buf}");
 }
